9542b7d7df40afb6a22993c60e3c3803b06ae4b8
Kennn-45 <kenshinamarta7@gmail.com>
2026-07-25 17:15:48 +0700

    Koding pertama.cpp

---
+++ Pertama.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl '\n'
+#define fastIo ios_base::stdio_with_sync(false) cin.tie(NULL)
+
+signed main () {
+  cout << "Hello World!" << endl;
+  return 0;
+}
